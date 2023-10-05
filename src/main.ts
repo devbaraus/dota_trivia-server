aad474eaea0408808f55c43ff0d16ccfbc2b0e95
Bruno de Araújo Alves <me@baraus.dev>
2023-10-04 22:01:30 -0300

    feat: add swagger doc
    Signed-off-by: Bruno de Araújo Alves <me@baraus.dev>

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as morgan from "morgan";
 
 import { AppModule } from "./app.module";
 
+const PORT = process.env.PORT || 4000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle("Dota Trivia")
+    .setDescription("The Dota Trivia API description")
+    .setVersion("0.1")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
+  app.use(morgan("tiny"));
+
+  await app.listen(PORT);
+  console.log(`Listening on port http://localhost:${PORT}`);
+  console.log(`Listening on port http://127.0.0.1:${PORT}`);
 }
+
 bootstrap();
